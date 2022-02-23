048f696727fd665b47d4bb22ece1b9b1cb703087
Lily O'Sullivan <lilyosullivan@pm.me>
2022-02-23 14:53:41 +0000

    ✨ Implemented some builder patterns

---
+++ Builder/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder
+{
+    public class HtmlElement
+    {
+        public string Name, Text;
+        public List<HtmlElement> Elements = new List<HtmlElement>();
+        private const int _indentSize = 2;
+
+        public HtmlElement()
+        {
+
+        }
+
+        public HtmlElement(string name, string text)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Text = text ?? throw new ArgumentNullException(nameof(text));
+        }
+
+        private string TOStringImpl(int indent)
+        {
+            var stringBuilder = new StringBuilder();
+            var i = new string(' ', _indentSize * indent);
+            stringBuilder.Append($"{i}<{Name}>\n");
+
+            if (!string.IsNullOrWhiteSpace(Text))
+            {
+                stringBuilder.Append(new string(' ', _indentSize * (indent + 1)));
+                stringBuilder.AppendLine(Text);
+            }
+
+            foreach (var e in Elements)
+            {
+                stringBuilder.Append(e.TOStringImpl(indent + 1));
+            }
+            stringBuilder.Append($"{i}</{Name}>\n");
+            return stringBuilder.ToString();
+        }
+
+        public override string ToString()
+        {
+            return TOStringImpl(0);
+        }
+    }
+
+    public class HtmlBuilder
+    {
+        HtmlElement root = new HtmlElement();
+        private readonly string _rootName;
+
+        public HtmlBuilder(string rootName)
+        {
+            this._rootName = rootName;
+            root.Name = rootName;
+        }
+
+        public HtmlBuilder AddChild(string childName, string childText)
+        {
+            var element = new HtmlElement(childName, childText);
+            root.Elements.Add(element);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+
+        public void Clear()
+        {
+            root = new HtmlElement { Name = _rootName };
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = "hello";
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append("<p>");
+            stringBuilder.Append(hello);
+            stringBuilder.Append("</p>");
+            Console.WriteLine(stringBuilder.ToString());
+
+            var words = new[] { "hello", "world" };
+            stringBuilder.Clear();
+            stringBuilder.Append("<ul>");
+            foreach (string word in words)
+            {
+                stringBuilder.AppendFormat("<li>{0}</li>", word);
+            }
+            stringBuilder.Append("</ul>");
+            Console.WriteLine(stringBuilder.ToString());
+
+            Console.WriteLine();
+
+            // Ordinary non-fluent builder
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "hello");
+            builder.AddChild("li", "world");
+            Console.WriteLine(builder.ToString());
+
+            // Fluent interface
+            var builderFluent = new HtmlBuilder("ul");
+            builder.AddChild("li", "hello").AddChild("li", "world");
+            Console.WriteLine(builder.ToString());
+        }
+    }
+}
---
+++ FluentBuilderInheritanceWithRecursiveGenerics/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace FluentBuilderInheritanceWithRecursiveGenerics
+{
+    public class Person
+    {
+        public string Name;
+        public string Position;
+
+        public class Builder:PersonJobBuilder<Builder>
+        {
+
+        }
+
+        public static Builder New => new Builder();
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
+        }
+    }
+
+    public abstract class PersonBuilder
+    {
+        protected Person person = new Person();
+
+        public Person Build()
+        {
+            return person;
+        }
+    }
+
+    public class PersonInfoBuilder<Self>:PersonBuilder
+        where Self: PersonInfoBuilder<Self>
+    {
+        public Self Called(string name)
+        {
+            person.Name = name;
+            return (Self)this;
+        }
+    }
+
+    public class PersonJobBuilder<Self> : PersonInfoBuilder<PersonJobBuilder<Self>>
+        where Self: PersonJobBuilder<Self>
+    {
+        public Self WorksAsA(string position)
+        {
+            person.Position = position;
+            return (Self) this;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var alex = Person.New.Called("Alex").WorksAsA("Chef").Build();
+            Console.WriteLine(alex.ToString());
+
+        }
+    }
+}
