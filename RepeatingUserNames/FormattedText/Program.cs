04e45a26a95b30a7c674928d057bcdbf301fb035
Lily O'Sullivan <lilyosullivan@pm.me>
2022-03-09 11:11:58 +0000

    ✨ Implemented Flyweight

---
+++ RepeatingUserNames/FormattedText/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormattedText
+{
+    public class FormattedText
+    {
+        private readonly string _plainText;
+        private bool[] capitalize;
+
+        public FormattedText(string plainText)
+        {
+            _plainText = plainText;
+            capitalize = new bool[plainText.Length];
+        }
+
+        public void Capitalize(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                capitalize[i] = true;
+            }
+        }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+            for (int i = 0; i < _plainText.Length; i++)
+            {
+                var c = _plainText[i];
+                stringBuilder.Append(capitalize[i] ? char.ToUpper(c) : c);
+            }
+            return stringBuilder.ToString();
+        }
+    }
+
+    public class BetterFormattedText
+    {
+        private string _plainText;
+        private List<TextRange> formatting = new();
+
+        public BetterFormattedText(string plainText)
+        {
+            _plainText = plainText;
+        }
+
+        public TextRange GetRange(int start, int end)
+        {
+            var range = new TextRange { Start = start, End = end };
+            formatting.Add(range);
+            return range;
+        }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+            for(var i = 0; i < _plainText.Length; i++)
+            {
+                var c = _plainText[i];
+                foreach(var range in formatting)
+                {
+                    if(range.Covers(i) && range.Capitalize)
+                    {
+                        c = char.ToUpper(c);
+                    }
+                    stringBuilder.Append(c);
+                }
+            }
+            return stringBuilder.ToString();
+        }
+
+        public class TextRange
+        {
+            public int Start, End;
+            public bool Capitalize, Bold, Italic;
+
+            public bool Covers(int position)
+            {
+                return position >= Start && position <= End;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var formattedText = new FormattedText("This is a brave new World");
+            formattedText.Capitalize(10, 15);
+            Console.WriteLine(formattedText.ToString());
+
+            var betterFormattedText = new BetterFormattedText("This is a brave new World");
+            betterFormattedText.GetRange(10, 15).Capitalize = true;
+            Console.WriteLine(betterFormattedText.ToString());
+        }
+    }
+}
---
+++ RepeatingUserNames/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.dotMemoryUnit;
+using NUnit.Framework;
+
+namespace RepeatingUserNames
+{
+    public class User
+    {
+        private string _fullName;
+
+        public User(string fullName)
+        {
+            _fullName = fullName;
+        }
+    }
+
+    public class User2
+    {
+        static List<string> strings = new();
+        private int[] names;
+
+        public string FullName => string.Join(" ", names.Select(i => strings[i]));
+
+        public User2(string fullName)
+        {
+            int getOrAdd(string s)
+            {
+                int i = strings.IndexOf(s);
+                if (i != -1)
+                {
+                    return i;
+                }
+                else
+                {
+                    strings.Add(s);
+                    return strings.Count - 1;
+                }
+            }
+            names = fullName.Split(' ').Select(getOrAdd).ToArray();
+        }
+    }
+
+    [TestFixture]
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+
+        [Test]
+        public void TestUser()
+        {
+            var firstNames = Enumerable.Range(0, 100).Select(_ => RandomString());
+            var lastNames = Enumerable.Range(0, 100).Select(_ => RandomString());
+
+            var users = new List<User>();
+
+            foreach (var firstName in firstNames)
+            {
+                foreach (var lastName in lastNames)
+                {
+                    users.Add(new User($"{firstNames} {lastNames}"));
+                }
+            }
+
+            ForceGC();
+
+            dotMemory.Check(memory =>
+            {
+                Console.WriteLine(memory.SizeInBytes);
+            });
+        }
+
+        public void TestUser2()
+        {
+            var firstNames = Enumerable.Range(0, 100).Select(_ => RandomString());
+            var lastNames = Enumerable.Range(0, 100).Select(_ => RandomString());
+
+            var users = new List<User2>();
+
+            foreach (var firstName in firstNames)
+            {
+                foreach (var lastName in lastNames)
+                {
+                    users.Add(new User2($"{firstNames} {lastNames}"));
+                }
+            }
+
+            ForceGC();
+
+            dotMemory.Check(memory =>
+            {
+                Console.WriteLine(memory.SizeInBytes);
+            });
+        }
+
+        private void ForceGC()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+
+        private string RandomString()
+        {
+            Random rand = new Random();
+            return new string(
+                Enumerable.Range(0, 10)
+                .Select(i => (char)('a' + rand.Next(26)))
+                .ToArray()
+            );
+        }
+    }
+}
