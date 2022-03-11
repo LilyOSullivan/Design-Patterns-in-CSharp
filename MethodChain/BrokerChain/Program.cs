b53825d245d4ce4c2d27c9a8f306a47c83e32023
Lily O'Sullivan <lilyosullivan@pm.me>
2022-03-11 15:10:07 +0000

    ✨ Implemented Chain of Responsibility

---
+++ MethodChain/BrokerChain/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace BrokerChain
+{
+    public class Game
+    {
+        public event EventHandler<Query> Queries;
+
+        public void PerformQuery(object sender, Query query)
+        {
+            Queries?.Invoke(sender, query);
+        }
+    }
+
+    public class Query
+    {
+        public string CreatureName;
+
+        public enum Argument
+        {
+            Attack, Defence
+        }
+
+        public Argument WhatToQuery;
+
+        public int Value;
+
+        public Query(string creatureName, Argument whatToQuery, int value)
+        {
+            CreatureName = creatureName ?? throw new ArgumentNullException(nameof(creatureName));
+            WhatToQuery = whatToQuery;
+            Value = value;
+        }
+    }
+
+    public class Creature
+    {
+        private Game game;
+        public string Name;
+        private int attack, defence;
+
+        public Creature(Game game, string name, int attack, int defence)
+        {
+            this.game = game ?? throw new ArgumentNullException(nameof(game));
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            this.attack = attack;
+            this.defence = defence;
+        }
+
+        public int Attack
+        {
+            get
+            {
+                var query = new Query(Name, Query.Argument.Attack, attack);
+                game.PerformQuery(this, query);
+                return query.Value;
+            }
+        }
+
+        public int Defence
+        {
+            get
+            {
+                var query = new Query(Name, Query.Argument.Defence, defence);
+                game.PerformQuery(this, query);
+                return query.Value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Attack} {Defence}";
+        }
+    }
+
+    public abstract class CreatureModifier : IDisposable
+    {
+        protected Game _game;
+        protected Creature _creature;
+
+        protected CreatureModifier(Game game, Creature creature)
+        {
+            _game = game ?? throw new ArgumentNullException(nameof(game));
+            _creature = creature ?? throw new ArgumentNullException(nameof(creature));
+            _game.Queries += Handle;
+        }
+
+        protected abstract void Handle(object sender, Query query);
+
+        public void Dispose()
+        {
+            _game.Queries -= Handle;
+        }
+    }
+
+    public class DoubleAttackModifier : CreatureModifier
+    {
+        public DoubleAttackModifier(Game game, Creature creature) : base(game, creature)
+        {
+        }
+
+        protected override void Handle(object sender, Query query)
+        {
+            if (query.CreatureName == _creature.Name
+                && query.WhatToQuery == Query.Argument.Attack
+            )
+            {
+                query.Value *= 2;
+            }
+        }
+    }
+
+    public class IncreaseDefenseModifier : CreatureModifier
+    {
+        public IncreaseDefenseModifier(Game game, Creature creature) : base(game, creature)
+        {
+        }
+
+        protected override void Handle(object sender, Query query)
+        {
+            if (query.CreatureName == _creature.Name
+                && query.WhatToQuery == Query.Argument.Defence
+            )
+            {
+                query.Value += 3;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var game = new Game();
+            var goblin = new Creature(game, "Strong Goblin", 2, 10);
+            Console.WriteLine(goblin);
+            using (new DoubleAttackModifier(game, goblin))
+            {
+                Console.WriteLine(goblin);
+                using (new IncreaseDefenseModifier(game, goblin))
+                {
+                    Console.WriteLine(goblin);
+                }
+            }
+            Console.WriteLine(goblin);
+
+        }
+    }
+}
---
+++ MethodChain/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace MethodChain
+{
+    public class Creature
+    {
+        public string Name;
+        public int Attack, Defence;
+
+        public Creature(string name, int attack, int defence)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Attack = attack;
+            Defence = defence;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Attack} {Defence}";
+        }
+    }
+
+    public class CreatureModifier
+    {
+        protected Creature _creature;
+        protected CreatureModifier next; // Linked-List like pattern
+
+        public CreatureModifier(Creature creature)
+        {
+            _creature = creature;
+        }
+
+        public void Add(CreatureModifier creatureModifier)
+        {
+            if (next != null)
+            {
+                next.Add(creatureModifier);
+            }
+            else
+            {
+                next = creatureModifier;
+            }
+        }
+
+        public virtual void Handle() => next?.Handle();
+    }
+
+    public class NoBonusesModifier : CreatureModifier
+    {
+        public NoBonusesModifier(Creature creature) : base(creature)
+        {
+        }
+
+        public override void Handle()
+        {
+            Console.WriteLine("Preventing Bonuses");
+        }
+    }
+
+    public class DoubleAttackModifier : CreatureModifier
+    {
+        public DoubleAttackModifier(Creature creature) : base(creature)
+        {
+        }
+
+        public override void Handle()
+        {
+            Console.WriteLine($"Doubling {_creature.Name}'s attack!");
+            _creature.Attack *= 2;
+            base.Handle();
+        }
+    }
+
+    public class IncreaseDefenceModifier : CreatureModifier
+    {
+        public IncreaseDefenceModifier(Creature creature) : base(creature)
+        {
+        }
+
+        public override void Handle()
+        {
+            Console.WriteLine($"Increasing {_creature.Name}'s defence!");
+            _creature.Defence += 3;
+            base.Handle();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var goblin = new Creature("Goblin", 2, 10);
+            Console.WriteLine(goblin);
+
+            var root = new CreatureModifier(goblin);
+
+            //root.Add(new NoBonusesModifier(goblin));
+            root.Add(new DoubleAttackModifier(goblin));
+            root.Add(new IncreaseDefenceModifier(goblin));
+
+            root.Handle();
+
+            Console.WriteLine(goblin);
+        }
+    }
+}
