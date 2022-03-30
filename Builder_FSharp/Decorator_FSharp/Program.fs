46c80120414562b246c01c6efb14104a6b87d305
Lily O'Sullivan <lilyosullivan@pm.me>
2022-03-30 15:41:22 +0100

    ✨ Implemented patterns in F#

---
+++ Builder_FSharp/Decorator_FSharp/Program.fs
@@ -0,0 +1,17 @@
+﻿open System.Diagnostics
+let doWork() =
+    printfn "Doing some work"
+
+let logger work name =
+    let stopwatch = Stopwatch.StartNew()
+    printfn "%s %s" "Entering function" name
+    work()
+    stopwatch.Stop()
+    printfn "Exiting method %s: %fs elapsed" name stopwatch.Elapsed.TotalSeconds
+    
+
+[<EntryPoint>]
+let main argv =
+    logger doWork "do_work"
+    //work()
+    0
---
+++ Builder_FSharp/Factory_FSharp/Program.fs
@@ -0,0 +1,24 @@
+﻿type ICountryInfo =
+    abstract member Capital: string
+
+type Country =
+    | USA
+    | UK
+with
+    static member Create = function
+        | "USA" | "America" -> USA
+        | "UK" | "England" -> UK
+        | _ -> failwith "No Such Country"
+
+let make country =
+    match country with
+    | USA -> {new ICountryInfo with
+              member x.Capital = "Washington"}
+    | UK -> {new ICountryInfo with
+              member x.Capital = "London"}
+[<EntryPoint>]
+let main argv =
+    let uk = Country.Create "UK"
+    let usa =  make Country.USA
+    printfn "%s" usa.Capital
+    0 // return an integer exit code
